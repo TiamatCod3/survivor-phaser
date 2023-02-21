e0252dc3ace918198b4752fe8860cffce3351f2d
TiamatCod3 <ti_matos@id.uff.br>
2023-02-21 10:30:43 -0300

    Map finished

--- src/MainScene.js
+++ src/MainScene.js
@@ -8,11 +8,21 @@ export default class MainScene extends Phaser.Scene {
 
     preload() {
         Player.preload(this);
+        this.load.image('tiles', 'src/assets/images/RPG Nature Tileset.png');
+        this.load.tilemapTiledJSON('map', 'src/assets/images/map.json');
+        // this.preload.image('tiles', './assets/images/RPG Nature Tileset.png');
+        // this.preload.tilemapTiledJSON('map', './assets/images/map.json');
     }
 
     create() {
-        this.player = new Player({scene: this.matter.world, x: 0, y: 0,texture: 'female',frame: 'townsfolk_f_idle_1'});
-        let textPlayer = new Player({scene: this.matter.world, x: 100, y: 100,texture: 'female',frame: 'townsfolk_f_idle_1'});
+        const map = this.make.tilemap({ key: 'map' });
+        const tileset = map.addTilesetImage('RPG Nature Tileset', 'tiles', 32, 32, 0, 0);
+        const layer1 = map.createLayer('Tile Layer 1',tileset,0,0);
+        const layer2 = map.createLayer('Tile Layer 2',tileset,0,0);
+        layer1.setCollisionByProperty({collides: true});
+        this.matter.world.convertTilemapLayer(layer1);
+        this.player = new Player({ scene: this.matter.world, x: 100, y: 100, texture: 'female', frame: 'townsfolk_f_idle_1' });
+        // let textPlayer = new Player({ scene: this.matter.world, x: 100, y: 100, texture: 'female', frame: 'townsfolk_f_idle_1' });
         this.player.inputKeys = this.input.keyboard.addKeys({
             up: Phaser.Input.Keyboard.KeyCodes.W,
             down: Phaser.Input.Keyboard.KeyCodes.S,
